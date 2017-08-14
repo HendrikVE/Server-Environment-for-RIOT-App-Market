f14029dea00d5162bea4372c9a38fcdea3081ec1
HendrikVE <hendrik1995@msn.com>
2017-08-15 00:43:05 +0200

    reading subdirectories as modules in to database

--- db_update.py
+++ db_update.py
@@ -8,15 +8,22 @@ db = MySQLdb.connect(config.db_config["host"], config.db_config["user"], config.
 # cursor object to execute queries
 db_cursor = db.cursor()
 
-for i in range(len(config.module_directories)):
-	for (dirpath, dirnames, filenames) in os.walk(config.path_root + config.module_directories[i]):
-		for dirname in dirnames:
-			#db_cursor.execute()
-			print "INSERT INTO modules (name, path, description) VALUES ('{!s}', '{!s}', '{!s}');".format(dirname, dirpath + dirname, " ")
+db_cursor.execute("TRUNCATE modules")
 
-for row in db_cursor.fetchall():
-	for data in row:
-		print data
+for i in range(len(config.module_directories)):
+	
+	path = config.path_root + config.module_directories[i] + "/"
+	
+	for item in os.listdir(path):
+		if not os.path.isfile(os.path.join(path, item)):
+			
+			# ignoring include directories
+			if item == "include":
+				continue
+			
+			db_cursor.execute("INSERT INTO modules (name, path, description) VALUES ('{!s}', '{!s}', '{!s}');".format(item, path + item + "/", " "))
+
+db.commit()
 
 db_cursor.close()
 db.close()
