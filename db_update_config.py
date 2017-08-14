8ff0a0021144b4d48e1045c24d91faaa39e720f2
HendrikVE <hendrik1995@msn.com>
2017-08-14 23:57:30 +0200

    now they are added...

---
+++ db_update_config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+db_config = {
+	"host"   : "localhost",
+	"user"   : "backend_script",
+	"passwd" : "password",
+	"db"     : "riot_os"
+}
+
+path_root = "RIOT/"
+
+module_directories = [
+	"sys",
+	"pkg",
+	"drivers"
+]
