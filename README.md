252aadf616c89025cb0a2c679455cf7f537dc30f
HendrikVE <hendrik1995@msn.com>
2017-08-15 14:37:55 +0200

    adding description in to database

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 * create database 'riot_devices' and 'riot_os'
 * add a new user 'backend_script' with following privileges only for 'riot_os' database (NOT GLOBAL!):
     * SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER
-* change password for 'backend_script' in riotam-backend/db_config.py and riotam-backend/setup/db_config.py
+* change password for 'backend_script' in riotam-backend/db_config.py and riotam-backend/setup/db_config.py by the password you set by creating user 'backend_script'
 * go to riotam-backend/setup and run 'python db_setup.py'
--- db_update.py
+++ db_update.py
@@ -4,27 +4,51 @@ import db_config as config
 import MySQLdb
 import os
 
-db = MySQLdb.connect(config.db_config["host"], config.db_config["user"], config.db_config["passwd"], config.db_config["db"])
-
-# cursor object to execute queries
-db_cursor = db.cursor()
-
-db_cursor.execute("TRUNCATE modules")
-
-for i in range(len(config.module_directories)):
+def main():
 	
-	path = config.path_root + config.module_directories[i] + "/"
+	db = MySQLdb.connect(config.db_config["host"], config.db_config["user"], config.db_config["passwd"], config.db_config["db"])
+
+	# cursor object to execute queries
+	db_cursor = db.cursor()
+
+	db_cursor.execute("TRUNCATE modules")
+
+	for i in range(len(config.module_directories)):
+
+		path = config.path_root + config.module_directories[i] + "/"
+
+		for item in os.listdir(path):
+			if not os.path.isfile(os.path.join(path, item)):
+
+				# ignoring include directories
+				if item == "include":
+					continue
+
+				description = get_description(path + item + "/doc.txt")
+				
+				sql = "INSERT INTO modules (name, path, description) VALUES (%s, %s, %s);"
+				db_cursor.execute(sql, (item, path + item + "/", description))
+
+	db.commit()
+
+	db_cursor.close()
+	db.close()
+
+def get_description(path):
 	
-	for item in os.listdir(path):
-		if not os.path.isfile(os.path.join(path, item)):
-			
-			# ignoring include directories
-			if item == "include":
-				continue
-			
-			db_cursor.execute("INSERT INTO modules (name, path, description) VALUES ('{!s}', '{!s}', '{!s}');".format(item, path + item + "/", " "))
+	description = ""
+	
+	try:
+		with open(path) as file:  
+			description = file.read()
+	except IOError:
+		# ignore missing doc.txt
+		return None
+		
+	if description == "":
+		description = None
+		
+	return description  
 
-db.commit()
-
-db_cursor.close()
-db.close()
+if __name__ == '__main__':
+	main()
