2b1f720927b6d7a876a6a392285ae29aa6a71f32
HendrikVE <hendrik1995@msn.com>
2017-08-30 21:54:29 +0200

    changed schema for devices and changed scripts for that purpose

--- db_update.py
+++ db_update.py
@@ -4,15 +4,28 @@ import db_config as config
 import MySQLdb
 import os
 
+db_cursor = None
+db = None
+
 def main():
+	
+	global db_cursor, db
 	
 	db = MySQLdb.connect(config.db_config["host"], config.db_config["user"], config.db_config["passwd"], config.db_config["db"])
 
 	# cursor object to execute queries
 	db_cursor = db.cursor()
 
-	db_cursor.execute("TRUNCATE modules")
+	update_modules()
+	update_devices()
 
+	db_cursor.close()
+	db.close()
+	
+def update_modules():
+	
+	db_cursor.execute("TRUNCATE modules")
+	
 	for i in range(len(config.module_directories)):
 
 		module_directory = config.module_directories[i]
@@ -31,9 +44,20 @@ def main():
 				db_cursor.execute(sql, (item, path + item + "/", description, module_directory))
 
 	db.commit()
+	
+def update_devices():
+	
+	db_cursor.execute("TRUNCATE devices")
+	
+	path = config.path_root + "boards/"
 
-	db_cursor.close()
-	db.close()
+	for item in os.listdir(path):
+		if not os.path.isfile(os.path.join(path, item)):
+			
+			sql = "INSERT INTO devices (display_name, internal_name, flash_program) VALUES (%s, %s, %s);"
+			db_cursor.execute(sql, (item, item, "openocd"))
+			
+	db.commit()
 
 def get_description(path):
 	
