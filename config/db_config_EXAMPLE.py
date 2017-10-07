3d59a5733c1a8fe94f5952983a57d2334646c8ab
HendrikVE <hendrik1995@msn.com>
2017-10-07 14:25:09 +0200

    added the example configs

---
+++ config/db_config_EXAMPLE.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+db_config = {
+    "host": "localhost",
+    "user": "riotam_backend",
+    "passwd": "PASSWORD",
+    "db": "riot_os"
+}
+
+path_root = "RIOT/"
+
+module_directories = [
+    "sys",
+    "pkg",
+    "drivers",
+    "tests"
+]
+
+application_directories = [
+    "examples"
+]
---
+++ setup/db_config_EXAMPLE.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+db_config = {
+    "host": "localhost",
+    "db": "riot_os",
+
+    "user_backend": "riotam_backend",
+    "passwd_backend": "PASSWORD#1",
+
+    "user_website": "riotam_website",
+    "passwd_website": "PASSWORD#2",
+}
