570b0fe53fccdbb1ec16235eb48d6fc74c4dce1a
HendrikVE <hendrik1995@msn.com>
2017-10-16 21:34:36 +0200

    added script to update backend

---
+++ riotam_backend/push_webhook_handler.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from __future__ import print_function
+
+import logging
+import os
+from subprocess import Popen, PIPE, STDOUT
+
+CUR_DIR = os.path.abspath(os.path.dirname(__file__))
+PATH_RIOTAM_BACKEND = os.path.normpath(os.path.join(CUR_DIR, ".."))
+
+
+def main():
+    """
+    Routine to update backend repository. Gets called by push_webhook_handler.py inside frontend"""
+
+    """UPDATE GIT REPOSITORY"""
+    output = execute_command(["git", "-C", PATH_RIOTAM_BACKEND, "pull"])
+    logging.debug("PULL BACKEND REPO:\n" + output)
+
+    output = execute_command(["git", "-C", PATH_RIOTAM_BACKEND, "submodule", "update", "--recursive", "--remote"])
+    logging.debug("UPDATE SUBMODULES:\n" + output)
+
+    """SETUP DATABASE"""
+    output = execute_command(["python", "db_create.py", "--user", "root", "--password", "eBXa1zG2jlfwt1P71Udy"], os.path.join(PATH_RIOTAM_BACKEND, "riotam_backend", "setup"))
+    logging.debug("DB_CREATE:\n" + output)
+
+    output = execute_command(["python", "db_setup.py"], os.path.join(PATH_RIOTAM_BACKEND, "riotam_backend", "setup"))
+    logging.debug("DB_SETUP:\n" + output)
+
+    """UPDATE DATABASE"""
+    output = execute_command(["python", "db_update.py"], os.path.join(PATH_RIOTAM_BACKEND, "riotam_backend", "tasks", "database"))
+    logging.debug("DB_UPDATE:\n" + output)
+
+    """CREATE STRIPPED RIOT REPOSITORY"""
+    output = execute_command(["python", "strip_riot_repo.py"], os.path.join(PATH_RIOTAM_BACKEND, "riotam_backend"))
+    logging.debug("STRIP_RIOT_REPO.py:\n" + output)
+
+
+def execute_command(cmd, cwd=None):
+    """
+    Execute command with Popen
+
+    Parameters
+    ----------
+    cmd: array_like
+        List of strings of the command to execute. Needs to be split on spaces
+    cwd: string
+        working directory to change to (default is None)
+
+    Returns
+    -------
+    string
+        Commandline output
+
+    """
+    process = Popen(cmd, stdout=PIPE, stderr=STDOUT, cwd=cwd)
+    return process.communicate()[0]
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(filename="log/push_webhook_handler_log.txt", format="%(asctime)s [%(levelname)s]: %(message)s",
+                        datefmt="%Y-%m-%d %H:%M:%S", level=logging.DEBUG)
+
+    try:
+        main()
+
+    except Exception as e:
+        logging.error(str(e), exc_info=True)
