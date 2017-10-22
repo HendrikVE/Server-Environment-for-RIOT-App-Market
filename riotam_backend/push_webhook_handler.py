34d837b1312a979c55ee7dde25f406b2fb6b3670
HendrikVE <hendrik1995@msn.com>
2017-10-22 23:19:54 +0200

    fixed paths

--- riotam_backend/push_webhook_handler.py
+++ riotam_backend/push_webhook_handler.py
@@ -8,7 +8,6 @@ import os
 from subprocess import Popen, PIPE, STDOUT
 
 CUR_DIR = os.path.abspath(os.path.dirname(__file__))
-PATH_RIOTAM_BACKEND = os.path.normpath(os.path.join(CUR_DIR, ".."))
 PROJECT_ROOT_DIR = os.path.normpath(os.path.join(CUR_DIR, ".."))
 
 LOGFILE = os.path.join(PROJECT_ROOT_DIR, "log", "push_webhook_handler_log.txt")
@@ -26,18 +25,18 @@ def main():
     logging.debug("UPDATE SUBMODULES:\n" + output)
 
     """SETUP DATABASE"""
-    output = execute_command(["python", "db_create.py"], os.path.join(PATH_RIOTAM_BACKEND, "riotam_backend", "setup"))
+    output = execute_command(["python", "db_create.py"], os.path.join(PROJECT_ROOT_DIR, "riotam_backend", "setup"))
     logging.debug("DB_CREATE:\n" + output)
 
-    output = execute_command(["python", "db_setup.py"], os.path.join(PATH_RIOTAM_BACKEND, "riotam_backend", "setup"))
+    output = execute_command(["python", "db_setup.py"], os.path.join(PROJECT_ROOT_DIR, "riotam_backend", "setup"))
     logging.debug("DB_SETUP:\n" + output)
 
     """UPDATE DATABASE"""
-    output = execute_command(["python", "db_update.py"], os.path.join(PATH_RIOTAM_BACKEND, "riotam_backend", "tasks", "database"))
+    output = execute_command(["python", "db_update.py"], os.path.join(PROJECT_ROOT_DIR, "riotam_backend", "tasks", "database"))
     logging.debug("DB_UPDATE:\n" + output)
 
     """CREATE STRIPPED RIOT REPOSITORY"""
-    output = execute_command(["python", "strip_riot_repo.py"], os.path.join(PATH_RIOTAM_BACKEND, "riotam_backend"))
+    output = execute_command(["python", "strip_riot_repo.py"], os.path.join(PROJECT_ROOT_DIR, "riotam_backend"))
     logging.debug("STRIP_RIOT_REPO.py:\n" + output)
 
     # give calling script from frontend an answer
